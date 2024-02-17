0b942f5ae0578c6835a36c024182cf28382ea64b
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-02-17 19:14:55 +0300

    button changes

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -4,13 +4,14 @@ const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
   return (
     <div className='product'>
-      <img src={productImage} />
       <div className='description'>
+      <img src={productImage} />
         <p>
             {productName}
         </p>
         <p> ${price}</p>
       </div>
+      <button className='addToCartBttn'>Add To Cart</button>
     </div>
   )
 }
